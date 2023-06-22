e7ff0ed3ba1861fdd508a59e4d4d57a70ef2f35e
Linas Vidziunas <linasvidz@simula.no>
2023-06-22 14:28:21 +0200

    Log suspicious statement used for the patch

--- src/main/java/edu/lu/uni/serval/tbar/TBarFixer.java
+++ src/main/java/edu/lu/uni/serval/tbar/TBarFixer.java
@@ -107,7 +107,10 @@ public class TBarFixer extends AbstractFixer {
 				if (!isTestFixPatterns && minErrorTest == 0) break;
 				if (this.patchId >= 10000) break;
 			}
-			if (!isTestFixPatterns && minErrorTest == 0) break;
+			if (!isTestFixPatterns && minErrorTest == 0) {
+				log.info("Suspcious statement used: " + suspiciousCode.classPath + "@" + suspiciousCode.lineNumber)
+				break;
+			}
 			if (this.patchId >= 10000) break;
         }
 		log.info("=======TBar: Finish off fixing======");
