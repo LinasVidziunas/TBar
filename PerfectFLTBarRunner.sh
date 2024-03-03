52cff167aedf52b21e9899ec288d50baaa3ea832
Linas Vidziunas <linasvidz@gmail.com>
2024-03-03 18:49:58 +0100

    📝 docs(scripts): Add usage description to shell scripts

--- PerfectFLTBarRunner.sh
+++ PerfectFLTBarRunner.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+USAGE=$(cat << 'EOF'
+Usage: ./PerfectFLTBarRunner.sh <bugDataPath> <bugID> <defects4jHome>
+
+Positional arguments:
+  <bugDataPath>       The path to the directory containing the bug data. (e.g. "D4J/projects/")
+  <bugID>             The ID of the bug to be analyzed. (e.g. "Lang_10")
+  <defects4jHome>     The path to the Defects4J installation. (e.g. "~/defects4j/")
+EOF
+)
+
+if [ "$#" -ne 3 ]; then
+    printf "%s\n" "$USAGE"
+    exit 1
+fi
+
+bugDataPath="$(realpath "$1")/"
 bugID=$2
 defects4jHome="$(realpath "$3")/"
 failedTestCasesDir="$(realpath "./FailedTestCases")/"
