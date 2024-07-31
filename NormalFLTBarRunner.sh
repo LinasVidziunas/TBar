541941f75ca468a99f73115e584093a91d81bed8
Linas Vidziunas <linasvidz@gmail.com>
2024-07-31 12:22:48 +0200

    🐛 fix(scripts): Quote variables in Java command to handle spaces
    Enclose path-related variables in double quotes in
    `NormalFLTBarRunner.sh` and `PerfectFLTBarRunner.sh` to ensure that
    paths containing values containing spaces or special characters are
    correctly handled.

--- NormalFLTBarRunner.sh
+++ NormalFLTBarRunner.sh
@@ -19,4 +19,4 @@ bugDataPath="$(realpath "$1")/"
 bugID=$2
 defects4jHome="$(realpath "$3")/"
 
-java -Xmx1g -cp "target/dependency/*" edu.lu.uni.serval.tbar.main.Main $bugDataPath $bugID $defects4jHome
+java -Xmx1g -cp "target/dependency/*" edu.lu.uni.serval.tbar.main.Main "${bugDataPath}" "${bugID}" "${defects4jHome}"
--- PerfectFLTBarRunner.sh
+++ PerfectFLTBarRunner.sh
@@ -21,4 +21,4 @@ defects4jHome="$(realpath "$3")/"
 failedTestCasesDir="$(realpath "./FailedTestCases")/"
 knownBugPositionsFilePath="$(realpath "./BugPositions.txt")"
 
-java -Xmx1g -cp "target/dependency/*" edu.lu.uni.serval.tbar.main.MainPerfectFL $bugDataPath $bugID $defects4jHome $failedTestCasesDir $knownBugPositionsFilePath
+java -Xmx1g -cp "target/dependency/*" edu.lu.uni.serval.tbar.main.MainPerfectFL "${bugDataPath}" "${bugID}" "${defects4jHome}" "${failedTestCasesDir}" "${knownBugPositionsFilePath}"
